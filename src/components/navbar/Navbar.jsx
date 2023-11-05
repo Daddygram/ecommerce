a7d47a1bec6d369f97e6e1d089a9923c8a1430b0
Tsotne Skhvitaridze <89193876+Daddygram@users.noreply.github.com>
2023-11-05 16:06:06 +0400

    hamburger-menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,28 +3,37 @@ import './Navbar.css'
 import { HiOutlineShoppingBag } from 'react-icons/hi2'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../context/ShopContext'
+import Burger from './Burger'
 
 const Navbar = () => {
+
+  const setActive = () => {
+    const hamMenu = document.getElementsByClassName('nav-burger')
+    hamMenu.classList.toggle('active')
+  }
 
   const [menu, setmenu] = useState("shop")
   const {getTotalCartItems} = useContext(ShopContext)
 
   return (
-    <div className="navbar">
+    <div className="navbar main-container">
+      <Burger onClick={()=>setActive()} />
+      <div className="nav-burger">
         <div className="nav-logo">
           <Link style={{ textDecoration: 'none', color: 'inherit' }} to={"/"}>YourLogo</Link>
         </div>
-        <ul className="nav-menu">
+          <ul className="nav-menu">
             <li onClick={()=>setmenu("shop")}><Link style={{ textDecoration: 'none', color: 'inherit' }} to={"/"}>Shop</Link>{menu=="shop"?<hr/>:<></>}</li>
             <li onClick={()=>setmenu("mens")}><Link style={{ textDecoration: 'none', color: 'inherit' }} to={"/men"}>Men</Link>{menu=="mens"?<hr/>:<></>}</li>
             <li onClick={()=>setmenu("womens")}><Link style={{ textDecoration: 'none', color: 'inherit' }} to={"/women"}>Women</Link>{menu=="womens"?<hr/>:<></>}</li>
             <li onClick={()=>setmenu("kids")}><Link style={{ textDecoration: 'none', color: 'inherit' }} to={"/kids"}>Kids</Link>{menu=="kids"?<hr/>:<></>}</li>
-        </ul>
-        <div className="nav-login-cart">
-          <Link style={{ textDecoration: 'none', color: 'inherit' }} to={"/login"}><button className='btn'>Login</button></Link>
-          <Link style={{ textDecoration: 'none', color: 'inherit' }} to={"/cart"}><HiOutlineShoppingBag size={"25px"} /></Link>
-          <div className="nav-cart-count">{getTotalCartItems()}</div>
-        </div>
+          </ul>
+        <Link style={{ textDecoration: 'none', color: 'inherit' }} to={"/login"}><button className='btn'>Login</button></Link>
+      </div>
+      <div className="nav-login-cart">
+        <Link style={{ textDecoration: 'none', color: 'inherit', position: 'relative'}} to={"/cart"}><HiOutlineShoppingBag size={"25px"} /></Link>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
+      </div>
     </div>
   )
 }
